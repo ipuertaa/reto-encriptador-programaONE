edce86f868f8065e0cf4801926fffea2a3f1cdbc
IsabelPuerta <isabel.mia2244@gmail.com>
2024-02-06 12:37:08 -0500

    Funcion encriptador
    Avance del reto encriptador. Ya funciona la función para encriptar. Falta terminar la de desencriptar

---
+++ encriptador.js
@@ -0,0 +1,77 @@
+            //Programa de JavaScript para el reto encriptador
+
+
+
+/* Para tener en cuenta: llaves de encriptación: 
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"  */ 
+
+
+//Función para encriptar texto
+function encriptarTexto(){
+    
+    let mensajeNoEncriptado = document.getElementById("textoUsuario").value;
+    let resultadoEncriptado = '';
+
+
+    for (var i = 0; i < mensajeNoEncriptado.length; i++){
+        switch (mensajeNoEncriptado[i]){
+            case 'a':
+                resultadoEncriptado += 'ai';
+                break;
+            
+            case 'e':
+                resultadoEncriptado += 'enter';
+                break;
+            
+            case 'i':
+                resultadoEncriptado += 'imes';
+                break;
+        
+            case 'o':
+                resultadoEncriptado += 'ober';
+                break;
+            
+            case 'u':
+                resultadoEncriptado += 'ufat';
+                break;
+            
+            default:
+                resultadoEncriptado += mensajeNoEncriptado[i];
+                break;
+        }
+    }
+    document.getElementById("imagen").style.display = "none";
+    asignarTextoElemento('espacioResultado', resultadoEncriptado);
+}
+
+
+//Función para asignar texto
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;     
+}
+
+//Función para desencriptar texto
+function desencriptarTexto(){
+    let mensajeEncriptado = document.getElementById("textoUsuario").value;
+    let resultadoDesencriptado = '';
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
